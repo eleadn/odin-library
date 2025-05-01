04be7088c92b5677d14ba304f4fce9162d43c28f
eleadn <eleano.dn@gmail.com>
2025-05-01 22:28:38 +0200

    Add libraryView class

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const cardContainers = document.querySelector('.container');
-
 class Book
 {
     #title;
@@ -38,12 +36,12 @@ class Library
 
     constructor(...books)
     {
-        this.#books = Array.from([...books]).map((book, _, __) => { id: crypto.randomUUID, book });
+        this.#books = Array.from([...books]).map(function(book, _, __) { return { id: crypto.randomUUID, content: book } });
     }
 
     addBook(book)
     {
-        this.#books.push({ id: crypto.randomUUID(), book });
+        this.#books.push({ id: crypto.randomUUID(), content: book });
     }
 
     removeBook(bookId)
@@ -53,7 +51,7 @@ class Library
 
     switchReadStatus(bookId)
     {
-        this.#books.find((book, _, __) => book.id === bookId).book.switchReadStatus();
+        this.#books.find((book, _, __) => book.id === bookId).content.switchReadStatus();
     }
 
     get getBooks()
@@ -62,52 +60,64 @@ class Library
     }
 }
 
-function displayBooks(library)
+class LibraryView
 {
-    while (cardContainers.hasChildNodes())
+    #container;
+
+    constructor(container)
     {
-        cardContainers.removeChild(cardContainers.firstChild);
+        this.#container = container;
     }
 
-    library.getBooks.forEach(item =>
+    display(library)
     {
-        const card = document.createElement("div");
-        card.dataset.id = item.id;
-        card.classList.add("card");
-        const name = document.createElement("h2");
-        name.textContent = item.book.title;
-        const author = document.createElement("h3");
-        author.textContent = `By ${item.book.author}`;
-        const pages = document.createElement("div");
-        pages.textContent = `${item.book.pages} pages`;
-        const readStatus = document.createElement("div");
-        readStatus.textContent = item.book.isRead ? "Finished reading" : "Not read yet";
-        const removeBtn = document.createElement("button");
-        removeBtn.textContent = "X";
-        removeBtn.addEventListener("click", event =>
+        while (this.#container.hasChildNodes())
         {
-            library.removeBook(event.target.parentNode.dataset.id);
-            displayBooks(library);
-        });
-        const switchReadBtn = document.createElement("button");
-        switchReadBtn.textContent = "Switch Read Status";
-        switchReadBtn.addEventListener("click", event =>
-        {
-            library.switchReadStatus(event.target.parentNode.dataset.id);
-            displayBooks(library);
-        });
+            this.#container.removeChild(this.#container.firstChild);
+        }
 
-        card.appendChild(removeBtn);
-        card.appendChild(name);
-        card.appendChild(author);
-        card.appendChild(pages);
-        card.appendChild(readStatus);
-        card.appendChild(switchReadBtn);
-        cardContainers.appendChild(card);
-    })
+        library.getBooks.forEach(book =>
+        {
+            const card = document.createElement("div");
+            card.dataset.id = book.id;
+            card.classList.add("card");
+            const name = document.createElement("h2");
+            name.textContent = book.content.title;
+            const author = document.createElement("h3");
+            author.textContent = `By ${book.content.author}`;
+            const pages = document.createElement("div");
+            pages.textContent = `${book.content.pages} pages`;
+            const readStatus = document.createElement("div");
+            readStatus.textContent = book.content.isRead ? "Finished reading" : "Not read yet";
+            const removeBtn = document.createElement("button");
+            removeBtn.textContent = "X";
+            removeBtn.addEventListener("click", event =>
+            {
+                library.removeBook(event.target.parentNode.dataset.id);
+                this.display(library);
+            });
+            const switchReadBtn = document.createElement("button");
+            switchReadBtn.textContent = "Switch Read Status";
+            switchReadBtn.addEventListener("click", event =>
+            {
+                library.switchReadStatus(event.target.parentNode.dataset.id);
+                this.display(library);
+            });
+
+            card.appendChild(removeBtn);
+            card.appendChild(name);
+            card.appendChild(author);
+            card.appendChild(pages);
+            card.appendChild(readStatus);
+            card.appendChild(switchReadBtn);
+            cardContainers.appendChild(card);
+        });
+    }
 }
 
+const cardContainers = document.querySelector('.container');
 const library = new Library();
+const libraryView = new LibraryView(cardContainers);
 
 const dialog = document.querySelector("dialog");
 const closeModal = document.querySelector(".close");
@@ -136,7 +146,7 @@ dialog.addEventListener("close", event =>
     }
 
     library.addBook(new Book(titleForm.value, authorForm.value, pagesForm.value, readForm.checked));
-    displayBooks(library);
+    libraryView.display(library);
     form.reset();
 });
 
@@ -152,4 +162,4 @@ submitButton.addEventListener("click", event =>
 
 library.addBook(new Book("The Hobbit", "J.R.R. Tolkien", 295, false));
 
-displayBooks(library);
+libraryView.display(library);
